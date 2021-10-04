a70502ceb7ccd033db39c1a94c5f8c79b1e0c735
BABYDAMINENI <57711864+BABYDAMINENI@users.noreply.github.com>
2021-10-04 19:41:19 +0530

    Update App.java

--- src/main/java/maven-groupid/App.java
+++ src/main/java/maven-groupid/App.java
@@ -1,1 +1,13 @@
+package maven-groupid;
 
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
